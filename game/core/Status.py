54ee11def2fa8c880dc3c019d872e4afef393979
The_Legolas <lucalegom@gmail.com>
2025-12-22 01:09:07 +0100

    I have finished everything to do with status effects. They have 1. been added to the game and additionally also to the ui 2. a lot of files have changed a bit to accomdate this 3. item definitions have change a bit to reflect by adding an effect list with all it's effects and if it has a status that have also been added 4. the Status class have gotton it's own file to stop circular confilcts as the _get_initiative_value function made it so both the Character_class and the Enemy_class files would try to import their respective classes which then tried to call status resulting in a circular loop, but it was stopped by moving status to another file 5. I added an inspect status to the inspect screen in the combat menu which also shows everything those statuses do  6. i changed combat_log_renderer to accomdate the status effects and added a turn counter to the battle report  7. I added a evaluate_status_magnitude which is currently bieng used figure out how many turn a status last but in the future it will be used for enemy decisions to evaluate what will deal the most amount of damage by also respecting the applied status effects. 8. I changed STATUS_REGISTRY to have clear order in the dict which goes as follows: id: priority, icon, stacking, max_stacks, description, modifiers.    I spent about 2 hours today.

---
+++ game/core/Status.py
@@ -0,0 +1,6 @@
+class Status():
+    def __init__(self, id: str, remaining_turns: int, magnitude: int | dict | None, source: str | None = None):
+        self.id = id
+        self.remaining_turns = remaining_turns
+        self.source = source
+        self.magnitude = magnitude
